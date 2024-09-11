5fbf98054884dfb45010fb9fdb75d29a9477a228
RamkumarSep <ramkumarvsep09@gmail.com>
2024-09-11 20:40:08 +0530

    First GIT Push

---
+++ FirstGIT.java
@@ -0,0 +1,24 @@
+package org.sample;
+
+public class FirstGIT {
+	
+	public void method1()
+	{
+		
+		System.out.println("Method 1");
+	}
+	
+	public void method2()
+	{
+		
+		System.out.println("Method 2");
+	}
+	
+	
+	public void method3()
+	{
+		
+		System.out.println("Method 3");
+	}
+
+}
